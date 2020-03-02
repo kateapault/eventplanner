1f518684c0b9dc13043db2f3a911180e14803456
kgc35 <kntchn@gmail.com>
2020-03-02 16:20:02 -0500

    Merging 419pm

--- frontend-eventplanner/index.js
+++ frontend-eventplanner/index.js
@@ -1,6 +1,9 @@
 url = 'http://localhost:3000/events'
 
 document.addEventListener("DOMContentLoaded", (event) => {
+
+    console.log("We are loaded now")
+    
     fetch(url)
     .then(response => { 
         response.json()
