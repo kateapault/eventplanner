573b2a72a078a86c0adf977595a36f5760708e3e
kgc35 <kntchn@gmail.com>
2020-03-02 15:28:32 -0500

    3:28pm

--- backend-eventplanner/app/controllers/ticket_controller.rb
+++ backend-eventplanner/app/controllers/ticket_controller.rb
@@ -1,4 +1,27 @@
 class TicketsController < ApplicationController
    def new
    end
+
+   def create
+    @ticket = Ticket.new(ticket_params);
+    
+    if @ticket.save
+      render json: @ticket.to_json
+    else
+      render json: ("Ticket creation unsuccessful").to_json
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    render json: ("Ticket successfully deleted")   # do we want to return the destroyed json for an undo option?
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:user_id, :event_id)
+  end
+
  end
--- backend-eventplanner/app/controllers/user_controller.rb
+++ backend-eventplanner/app/controllers/user_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-   def new
-   end
+  def new
+  end
+
+  def index
+    @users = Users.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id]);
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params);
+    
+    if @user.save
+      render json: @user.to_json
+    else
+      render json: ("User creation unsuccessful").to_json
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: ("User successfully deleted")   # do we want to return the destroyed json for an undo option?
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :age, :email, :phone_number)
+  end
+
+
  end
