c1e2f86daa8520d279fe6818c674d5bd2a6fd33c
Kate Raskauskas <kateapault@gmail.com>
2020-03-02 20:11:08 -0500

    added nav bar and partial create form but didn't hook up back end

--- frontend-eventplanner/index.js
+++ frontend-eventplanner/index.js
@@ -3,7 +3,7 @@ let eventsURL = `${baseURL}/events`
  
 document.addEventListener("DOMContentLoaded", () => {
     console.log("We are loaded now")
-
+    // document.querySelector('#create-event').style.display = "none"
     let events = fetch(eventsURL)
     .then(response => response.json())  
     .then(eventsJSON);
@@ -14,9 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
         showEvents(resp)
     }
 
+
+
 })
     
-
+///////////////////////////////////////
+///////////////////////////////////////
 
 function showEvents(eventsJSON){
     let showEventsDiv = document.querySelector('#show-events')
@@ -34,7 +37,7 @@ function eventDisplay(eventJSON){
     eventElement.setAttribute('data-user-id',eventJSON["user_id"])
     eventElement.className = "event-list"
     
-    if (!!eventJSON["img_url"]) {
+    if (eventJSON["img_url"]) {
         let eventImage = document.createElement('img')
         eventImage.setAttribute('src',eventJSON["img_url"])
         eventElement.appendChild(eventImage)
@@ -54,13 +57,13 @@ function eventDisplay(eventJSON){
     eventDateAndTime.innerText += ` | End: ${eventJSON["end_time"]}`
     eventElement.appendChild(eventDateAndTime)
 
-    if (!!eventJSON["min_age"]){
+    if (eventJSON["min_age"]){
         let eventMaxAttendees = document.createElement('p')
         eventMaxAttendees.innerText = `Minimum Age: ${eventJSON["min_age"]}`
         eventElement.appendChild(eventMaxAttendees)
     }
 
-    if (!!eventJSON["max_attendees"]){
+    if (eventJSON["max_attendees"]){
         let eventMaxAttendees = document.createElement('p')
         eventMaxAttendees.innerText = `Total Tickets: ${eventJSON["max_attendees"]}`
         console.log("this value is not null!")
@@ -73,4 +76,31 @@ function eventDisplay(eventJSON){
     eventElement.appendChild(buyTicketButton)
 
     return eventElement
+}
+
+
+////////////////////////////////
+////////////////////////////////
+
+function showEventForm() {
+    document.querySelector('#show-events')
+    document.querySelector('#create-event')
+    let today = new Date
+    let dd = today.getDate()
+    dd.length === 1 ? (dd = '0' + dd) : (dd)
+    let mm = today.getMonth()
+    mm.length === 1 ? (mm = '0' + mm) : (mm)
+    let yyyy = today.getFullYear()
+    document.querySelector('#date').setAttribute('max',`${yyyy}-${mm}-${dd}`)
+}
+
+function turnOffDivsExcept(divIdToKeep) {
+    let divs = document.querySelectorAll("body>div")
+    divs.forEach( div => {
+        if (div.id === "nav" || div.id === divIdToKeep) {
+            div.style.display = "block"
+        } else {
+            div.style.display = "none"
+        }
+    })
 }
